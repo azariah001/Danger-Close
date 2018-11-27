3097727167eb14f6327d74fc73b8081dfaf3be56
Azarel Howard <azariah001@gmail.com>
2018-11-27 23:44:44 +1000

    changes to core game.js class file

---
+++ snippets/game.js
@@ -0,0 +1,40 @@
+class Game {
+    constructor (template) {
+        this.template = template;
+        this.character_ids = [];
+        this.characters = { /* _id: new Character() */ };
+        this.dice = new Dice(template.dice);
+        this.currency = new Currency(template.currency);
+
+        this.attributeNames = template.attributeNames;
+        this.secondaryAttributes = template.secondaryAttributes;
+        this.secondaryAttributesCount = template.secondaryAttributesCount;
+        this.primaryAttributeCount = template.primaryAttributeCount;
+        //TODO Labels 
+    }
+    
+    addCharacter() {
+        this.characters.push(new Character[this.template.characters]);
+    }
+
+    removeCharacter(id) {
+        // Programmatic context
+        // deinitialize character class
+        delete this.characters[id];
+
+        // Character data context: db.characters
+        //TODO delete in MongoDB
+
+        // Game data context: db.games[_id}
+        // get index of character_id to be deleted
+        const index = this.character_ids.indexOf(id);
+
+        // ensure it exists (it always will)
+        if (index !== -1) {
+            this.character_ids.splice(index, 1);
+        }
+
+        // push game data changes to Mongo
+        //TODO update game document
+    }
+}
