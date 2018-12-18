f4690bb27b3e31facf0639e96233493319eb597a
Azarel Howard <azariah001@gmail.com>
2018-12-18 23:54:50 +1000

    updates to game.js class

--- snippets/game.js
+++ snippets/game.js
@@ -1,71 +1,40 @@
-<<<<<<< Updated upstream
 class Game {
-    constructor (template) {
-        this.template = template;
-        this.character_ids = [];
-        this.characters = { /* _id: new Character() */ };
-        this.dice = new Dice(template.dice);
-        this.currency = new Currency(template.currency);
+  constructor(template) {
+    this.template = template;
+    this.character_ids = [];
+    this.characters = { /* _id: new Character() */ };
+    this.dice = new Dice(template.dice);
+    this.currency = new Currency(template.currency);
 
-        this.attributeNames = template.attributeNames;
-        this.secondaryAttributes = template.secondaryAttributes;
-        this.secondaryAttributesCount = template.secondaryAttributesCount;
-        this.primaryAttributeCount = template.primaryAttributeCount;
-        //TODO Labels 
-    }
-    
-    addCharacter() {
-        this.characters.push(new Character[this.template.characters]);
-    }
+    this.attributeNames = template.attributeNames;
+    this.secondaryAttributes = template.secondaryAttributes;
+    this.secondaryAttributesCount = template.secondaryAttributesCount;
+    this.primaryAttributeCount = template.primaryAttributeCount;
+    //TODO Labels
+  }
 
-    removeCharacter(id) {
-        // Programmatic context
-        // deinitialize character class
-        delete this.characters[id];
+  addCharacter() {
+    this.characters.push(new Character[this.template.characters]);
+  }
 
-        // Character data context: db.characters
-        //TODO delete in MongoDB
+  removeCharacter(id) {
+    // Programmatic context
+    // deinitialize character class
+    delete this.characters[id];
 
-        // Game data context: db.games[_id}
-        // get index of character_id to be deleted
-        const index = this.character_ids.indexOf(id);
+    // Character data context: db.characters
+    //TODO delete in MongoDB
 
-        // ensure it exists (it always will)
-        if (index !== -1) {
-            this.character_ids.splice(index, 1);
-        }
+    // Game data context: db.games[_id}
+    // get index of character_id to be deleted
+    const index = this.character_ids.indexOf(id);
 
-        // push game data changes to Mongo
-        //TODO update game document
-    }
-=======
-class Game {
-    constructor (template) {
-        this.template = template;
-        this.character_ids = [];
-        this.characters = { /* _id: new Character() */ };
-        this.dice = new Dice(template.dice);
-        this.currency = new Currency(template.currency);
-
-        this.attributeNames = template.attributeNames;
-        this.secondaryAttributes = template.secondaryAttributes;
-        this.secondaryAttributesCount = template.secondaryAttributesCount;
-        this.primaryAttributeCount = template.primaryAttributeCount;
-        //TODO Labels 
-    }
-    
-    addCharacter() {
-        this.characters.push(new Character[this.template.characters]);
+    // ensure it exists (it always will)
+    if (index !== -1) {
+      this.character_ids.splice(index, 1);
     }
 
-    removeCharacter(id) {
-        delete this.characters[id];
-
-        //TODO delete in MongoDB
-
-        this.character_ids.indexOf(id);
-
-        //TODO update game document
-    }
->>>>>>> Stashed changes
+    // push game data changes to Mongo
+    //TODO update game document
+  }
 }
